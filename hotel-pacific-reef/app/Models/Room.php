679a51ff238bf6267ac29643b6bcf53505b84067
Vicente Elgueta <vice.elgueta@duocuc.cl>
2026-05-02 15:51:38 -0400

    arreglo errores

---
+++ hotel-pacific-reef/app/Models/Room.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'capacity',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'price' => 'float',
+        'capacity' => 'integer',
+    ];
+}
